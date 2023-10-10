95c72bf068a60ffa15209733363325650a1ac794
ameltj17 <amelpermatasari17@gmail.com>
2023-10-10 15:31:30 +0700

    first commit

---
+++ belajar_1/app/src/main/java/com/example/belajar_1/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.example.belajar_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set the initial layout to the linear layout
+        setContentView(R.layout.activity_home);
+
+        Button switchToLinearButton = findViewById(R.id.toRelativeBtn);
+        switchToLinearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
